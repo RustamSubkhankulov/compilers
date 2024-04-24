370d9387926701a6a195741ec4818a39e0bd4627
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-04-24 17:41:04 +0300

    few minor improvements

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -9,6 +9,7 @@ std::vector<std::unique_ptr<Token>> Tokens_parsed;
 const std::vector<std::unique_ptr<Token>>& get_tokens() {
 
   ::yylex();
+  Tokens_parsed.push_back(std::make_unique<Token_punct>(Token_punct::END));
   return Tokens_parsed;
 }
 
